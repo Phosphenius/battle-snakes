9e37b0660a3c147d497821d79b4128d7a9351614
Phosphenius <luca.kredel@web.de>
2016-11-10 17:00:58 +0100

    Tweak bot a little so it's no longer overpowered

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,7 @@ Snake AI powered by a Finite State Machine and Potential Fields.
 
 from abc import abstractmethod
 from heapq import nsmallest
+from copy import copy
 
 from player import PlayerBase
 from fsm import State, FiniteStateMachine
@@ -39,33 +40,41 @@ class BotCollectState(BotState):
         self.prev_target = None
         self.target = None
         self.path = []
+        self.pwrup_table = []
+
+        self.snake = bot.snake
+        self.game = bot.game
+
+        self.t_alive = 0
 
     def update(self, delta_time):
         self.prev_target = self.target
-        pwrup_table = []
-        for pwrup in self.bot.game.pwrup_manager.get_powerups():
-            dis = m_distance(self.bot.snake.body[0], pwrup.pos)
+        self.pwrup_table = []
+        for pwrup in self.game.pwrup_manager.get_powerups():
+            dis = m_distance(self.snake.body[0], pwrup.pos)
             score = self.bot.pwrup_score[pwrup.pid]
 
-            pwrup_table.append((dis, score, pwrup))
+            self.pwrup_table.append((dis, score, pwrup))
 
-        self.target = nsmallest(1, pwrup_table)[0][2].pos
+        if not self.target:
+            self.aquire_target()
+
+        if self.target.pos not in \
+                [elem[2].pos for elem in self.pwrup_table]:
+            self.aquire_target()
 
         if self.prev_target != self.target:
-            self.path = self.bot.pathfinder.find_path(
-                self.bot.snake.body[0],
-                self.target)
-            self.path.insert(0, self.target)
-            heading = sub_vecs(self.path[-1],
-                               self.bot.snake.body[0])
+            self.path = self.bot.pathfinder.find_path(self.snake[0],
+                                                      self.target.pos)
+            self.path.insert(0, self.target.pos)
+            heading = sub_vecs(self.path[-1], self.snake[0])
             self.bot.snake.set_heading(heading)
 
         if len(self.path) >= 1:
-            if self.path[-1] == self.bot.snake.body[0]:
+            if self.path[-1] == self.snake[0]:
                 self.path.pop()
 
-                heading = sub_vecs(self.path[-1],
-                                   self.bot.snake.body[0])
+                heading = sub_vecs(self.path[-1], self.snake[0])
                 self.bot.snake.set_heading(heading)
 
     def enter(self):
@@ -73,6 +82,10 @@ class BotCollectState(BotState):
 
     def leave(self):
         pass
+
+    def aquire_target(self):
+        selection = nsmallest(4, self.pwrup_table)
+        self.target = copy(self.game.randomizer.choice(selection)[2])
 
 
 class BotAttackState(BotState):
